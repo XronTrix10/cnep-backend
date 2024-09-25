31435cea84595e0f01cfd6d782da5b3df53b9d03
Xron Trix <xrontrix@gmail.com>
2024-09-25 16:05:54 +0530

    feat: utiliy functions to generate and validate JWT

---
+++ pkg/utils/jwt.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"time"
+
+	"cnep-backend/source/config"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(userID uint) (string, error) {
+	cfg := config.New()
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 30)), // Token expires in 30 days
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(cfg.JWTSecret))
+}
+
+func ValidateJWT(tokenString string) (uint, error) {
+	cfg := config.New()
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(cfg.JWTSecret), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if userID, ok := claims["user_id"].(float64); ok {
+			return uint(userID), nil
+		}
+		return 0, jwt.ErrInvalidKey
+	}
+
+	return 0, jwt.ErrSignatureInvalid
+}
