3790eed39047b5a0b57c9712fa7b5149d0543200
Xron Trix <xrontrix@gmail.com>
2024-09-25 16:06:38 +0530

    feat: utiliy function to validate user inputs

---
+++ pkg/utils/validation.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"regexp"
+	"unicode"
+)
+
+func IsValidEmail(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(email)
+}
+
+func IsValidPassword(password string) bool {
+	var (
+		hasUpper   = false
+		hasLower   = false
+		hasNumber  = false
+		hasSpecial = false
+	)
+
+	if len(password) < 8 {
+		return false
+	}
+
+	for _, char := range password {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+
+	return hasUpper && hasLower && hasNumber && hasSpecial
+}
