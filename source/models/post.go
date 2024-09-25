342af1762c49f570293b212883838e1042fb82d8
Xron Trix <xrontrix@gmail.com>
2024-09-26 00:51:25 +0530

    fix: post model update

--- source/models/post.go
+++ source/models/post.go
@@ -7,7 +7,8 @@ import (
 type Post struct {
 	ID         uint      `gorm:"primaryKey" json:"id"`
 	UserID     uint      `gorm:"not null" json:"user_id"`
-	PostType   string    `gorm:"not null;check:post_type IN ('regular', 'request')" json:"post_type"`
+	IsRequest  bool      `gorm:"not null" json:"is_request"`
+	IsUrgent   bool      `gorm:"not null" json:"is_urgent"`
 	Status     string    `gorm:"not null;check:status IN ('accepted', 'completed', 'pending', 'urgent')" json:"status"`
 	AssignedTo uint      `json:"assigned_to"`
 	Content    string    `gorm:"not null" json:"content"`
