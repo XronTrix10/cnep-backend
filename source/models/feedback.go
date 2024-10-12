271f48d70ebd6af7e8bc5a6787331a9476a0ab21
Xron Trix <xrontrix@gmail.com>
2024-10-13 00:42:12 +0530

    feat: added model for feedback

---
+++ source/models/feedback.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Feedback struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	SenderID   uint      `gorm:"not null" json:"sender_id"`
+	ReceiverID uint      `gorm:"not null" json:"receiver_id"`
+	Content    string    `gorm:"not null" json:"content"`
+	Rating     uint8     `gorm:"not null" json:"rating"`
+	CreatedAt  time.Time `gorm:"default:current_timestamp" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"default:current_timestamp" json:"updated_at"`
+	Sender     User      `gorm:"foreignKey:SenderID" json:"sender"`
+	Receiver   User      `gorm:"foreignKey:ReceiverID" json:"receiver"`
+}
